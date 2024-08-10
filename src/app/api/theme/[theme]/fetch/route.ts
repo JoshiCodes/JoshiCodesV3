fcf339ffc4ecfbb4e4343218e902fc2053fca586
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2024-08-10 17:09:01 +0200

    temporary solution for a weird error

--- src/app/api/theme/[theme]/fetch/route.ts
+++ src/app/api/theme/[theme]/fetch/route.ts
@@ -1,5 +1,6 @@
 import {NextRequest} from "next/server";
-import {fetchTheme} from "@/themes/ThemeProvider";
+import {readdirSync, readFileSync} from "node:fs";
+import {DEFAULT_THEME} from "@/themes/ThemeProvider";
 
 export async function GET(request: NextRequest, { params } : { params: { theme: string } }) {
 
@@ -26,4 +27,25 @@ export async function GET(request: NextRequest, { params } : { params: { theme: 
         }
     )
 
+}
+
+async function fetchTheme(theme: string): Promise<Object|null> {
+    // Check if theme is found in the themes folder
+    const themes = readdirSync("./public/static/themes");
+    if(!themes.includes(theme.toLowerCase())) {
+        theme = DEFAULT_THEME;
+    }
+
+    const folder = "./public/static/themes/" + theme.toLowerCase();
+    const dir = readdirSync(folder);
+
+    if(!dir || !dir.includes("theme.json") || !dir.includes("style.css")) {
+        console.log("Theme " + theme + " is missing files");
+        if(theme.toLowerCase() !== DEFAULT_THEME) {
+            return await fetchTheme(DEFAULT_THEME);
+        }
+        return null;
+    }
+    const buffer = readFileSync(folder + "/theme.json");
+    return JSON.parse(buffer.toString());
 }
--- src/app/api/theme/themes/route.ts
+++ src/app/api/theme/themes/route.ts
@@ -1,5 +1,5 @@
-import {readdirSync} from "node:fs";
-import {fetchTheme} from "@/themes/ThemeProvider";
+import {readdirSync, readFileSync} from "node:fs";
+import {DEFAULT_THEME} from "@/themes/ThemeProvider";
 
 export async function GET(request: Request) {
     // Get all theme names from the themes folder.
@@ -50,4 +50,25 @@ export async function GET(request: Request) {
             }
         }
     )
+}
+
+async function fetchTheme(theme: string): Promise<Object|null> {
+    // Check if theme is found in the themes folder
+    const themes = readdirSync("./public/static/themes");
+    if(!themes.includes(theme.toLowerCase())) {
+        theme = DEFAULT_THEME;
+    }
+
+    const folder = "./public/static/themes/" + theme.toLowerCase();
+    const dir = readdirSync(folder);
+
+    if(!dir || !dir.includes("theme.json") || !dir.includes("style.css")) {
+        console.log("Theme " + theme + " is missing files");
+        if(theme.toLowerCase() !== DEFAULT_THEME) {
+            return await fetchTheme(DEFAULT_THEME);
+        }
+        return null;
+    }
+    const buffer = readFileSync(folder + "/theme.json");
+    return JSON.parse(buffer.toString());
 }
